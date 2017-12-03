d4648ff14700d00d510a745ff25ee91b157521a6
akkgr <akkgr@users.noreply.github.com>
2017-12-03 06:42:05 +0200

    remove font awsome from src

--- src/core/Nav.js
+++ src/core/Nav.js
@@ -4,11 +4,11 @@ import { Route, NavLink } from 'react-router-dom'
 class Nav extends Component {
   render() {
     return (
-      <nav className='navbar is-dark' role='navigation'>
+      <nav className='navbar is-dark'>
         <div className='navbar-brand'>
           <a className='navbar-item'>
             <span className='icon'>
-            <i className='fa fa-bars'></i>
+              <i className='fa fa-bars'></i>
             </span>
           </a>
           <NavLink className='navbar-item' to='/'>IRIS</NavLink>
