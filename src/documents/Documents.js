dfd7b330a782d06c9f2d090f1a8a0eb06cdae857
akhellas <ak.hellas@gmail.com>
2018-01-09 10:28:16 +0200

    wip

--- src/documents/Documents.js
+++ src/documents/Documents.js
@@ -7,6 +7,24 @@ import CreateDocument from './CreateDocument'
 
 const Documents = () => (
   <div className="is-fullheight">
+    {/* <nav class="breadcrumb is-centered" aria-label="breadcrumbs">
+      <ul>
+        <li>
+          <a href="#">Bulma</a>
+        </li>
+        <li>
+          <a href="#">Documentation</a>
+        </li>
+        <li>
+          <a href="#">Components</a>
+        </li>
+        <li class="is-active">
+          <a href="#" aria-current="page">
+            Breadcrumb
+          </a>
+        </li>
+      </ul>
+    </nav> */}
     <Switch>
       <Route exact path="/documents" component={DocumentsDashboard} />
       <Route exact path="/documents/new" component={CreateDocument} />
